d53358090db98972ead0338e26c500b8aa52a83c
dirtyb1rd <yeskov777.888@gmail.com>
2024-10-03 20:32:12 +0300

    first commit

---
+++ part1/music-collection.js
@@ -0,0 +1,27 @@
+const musicCollection = {
+  albums: [
+    { title: "Abbey Road", artist: "The Beatles", year: 1969 },
+    { title: "Thriller", artist: "Michael Jackson", year: 1982 },
+    { title: "Back in Black", artist: "AC/DC", year: 1980 },
+    { title: "The Dark Side of the Moon", artist: "Pink Floyd", year: 1973 }
+  ],
+  
+  [Symbol.iterator]() {
+    let index = 0;
+    const albums = this.albums;
+
+    return {
+      next() {
+        if (index < albums.length) {
+          return { value: albums[index++], done: false };
+        } else {
+          return { done: true };
+        }
+      }
+    };
+  }
+};
+
+for (const album of musicCollection) {
+  console.log(`${album.title} - ${album.artist} (${album.year})`);
+}
